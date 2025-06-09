5535a15207716aa41c51c83903305b447ea2b238
Santiago Zapata Barahona <llsanthiago@gmail.com>
2025-06-09 01:24:09 -0500

    fix: correct section class for layout and adjust padding for improved design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 
 export default function Home() {
     return (
-        <section className="w-screen h-screen absoulte top-0">
-            <div className="m-auto w-fit my-auto flex gap-4 flex-wrap md:flex-nowrap pt-4">
+        <section className="w-full h-screen max-w-screen overflow-hidden">
+            <div className="m-auto w-fit my-auto flex gap-4 flex-wrap md:flex-nowrap pt-16 md:pt-0">
                 <div className='flex flex-col justify-center'>
                     <hgroup className="mb-6">
                         <h2 className={clsx("text-5xl font-bold mb-2 text-accent font-bol text-balance", fontPrimary.className)}>
@@ -26,12 +26,13 @@ export default function Home() {
                         </Button>
                     </Link>
                 </div>
-                <div>
+                <div className='overflow-hidden'>
                     <Image
                         src="/hero.webp"
                         width={1024}
                         height={1024}
                         alt='Two girls rating events'
+                        className='object-center object-contain'
                     />
                 </div>
             </div>
